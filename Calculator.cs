0956d1295cbd38fd341308316e2657e33c1b7c0f
khalil <khalil@students.amikom.ac.id>
2025-03-13 10:09:12 +0700

    Menambahkan operasi pengurangan

--- Calculator.cs
+++ Calculator.cs
@@ -4,9 +4,19 @@ class Calculator
 	{
 		int a = 10;
 		int b = 6;
+		
+		Consol.Writeline("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+		Consol.Writeline("Hasil Penambahan: {0} - {1} = {2}", a, b, Pengurangan, b));
+		
+		Cosole.Writeline("\n Tekan sembarang key untuk keluar");
+		Console.ReadKey();
 	}
 	static int Penambahan(int a, int b)
 	{
 		return a + b;
+	}
+	statuic int Pengurangan(int a, int b)
+	{
+		return a - b;
 	}
 }
